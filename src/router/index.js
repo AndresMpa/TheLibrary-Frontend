48e1e5ef89e5487d73ec704a49d364dd6f990fc2
Andres M. Prieto <andresprieto2703@gmail.com>
2022-04-17 04:39:57 -0500

    Adding routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,18 +5,70 @@ import Main from "../views/Main.vue";
 Vue.use(VueRouter);
 
 const routes = [
+  // Client
+
   {
     path: "/",
     name: "Main",
     component: Main,
+    meta: {
+      public: true,
+    },
+    children: [
+      {
+        path: "/store",
+        name: "Store",
+        component: () => import("../views/Store.vue"),
+      },
+      {
+        path: "/news",
+        name: "News",
+        component: () => import("../views/News.vue"),
+      },
+      {
+        path: "/history",
+        name: "History",
+        component: () => import("../views/History.vue"),
+      },
+      {
+        path: "/buying",
+        name: "Buying",
+        component: () => import("../views/Buying.vue"),
+      },
+      {
+        path: "/profile",
+        name: "Profile",
+        component: () => import("../views/Profile.vue"),
+      },
+    ],
   },
+
+  // Management (Administrator)
+
+  {
+    path: "/management",
+    name: "Management",
+    component: () => import("../views/Management.vue"),
+  },
+
+  {
+    path: "/add-book",
+    name: "AddBook",
+    component: () => import("../views/AddBook.vue"),
+  },
+
+  // Login
+
   {
     path: "/login",
     name: "Login",
-    component: () =>
-      import("../views/Log.vue"),
-    component: Main,
-  }
+    component: () => import("../views/Log.vue"),
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: () => import("../views/Register.vue"),
+  },
 ];
 
 const router = new VueRouter({

f75f487a8c6750faaae9227084a7efb18c5b2ac8
Andres M. Prieto <andresprieto2703@gmail.com>
2022-04-17 04:42:08 -0500

    Some utils
    Adding a paginator and making less dump the content loader

---
+++ src/components/util/ContentLoader.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid >
+    <v-slide-y-transition mode="out-in">
+      <router-view />
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ContentLoader",
+};
+</script>
---
+++ src/components/util/Pagination.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-center">
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="8">
+          <v-container class="max-width">
+            <v-pagination
+              v-model="page"
+              :length="15"
+              class="my-4"
+              circle
+            ></v-pagination>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pagination",
+  data() {
+    return {
+      page: 1,
+    };
+  },
+};
+</script>
+
+<style lang="css" scoped></style>

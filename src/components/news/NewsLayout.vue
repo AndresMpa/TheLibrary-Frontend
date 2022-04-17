ef20aa1849dc5a313a3a71ca5884115e56e8951b
Andres M. Prieto <andresprieto2703@gmail.com>
2022-04-17 04:44:28 -0500

    Starting news content

---
+++ src/components/news/NewsLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <article>
+    <Pagination />
+  </article>
+</template>
+
+<script>
+import Pagination from "../util/Pagination.vue";
+export default {
+  components: { Pagination },
+  name: "NewsLayout",
+};
+</script>
+
+<style lang="css" scoped></style>

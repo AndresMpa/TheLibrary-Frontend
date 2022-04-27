18cb85bf01f2bcac02c3a43bb0786f4e8fe0a793
Andres M. Prieto <andresprieto2703@gmail.com>
2022-04-27 00:20:23 -0500

    Split
    Split on controllers

---
+++ src/components/history/HistoryController.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card class="mx-auto" color="alter" dark>
+    <v-card-title class="text-h5">
+      Historial de compras de {{ this.$store.state.user }}
+    </v-card-title>
+    <v-card-text> Aquí puedes consultas tu hisorial de compras </v-card-text>
+    <v-card-actions>
+      <v-btn small class="yellow darken-3">
+        <v-icon> mdi-script-text </v-icon>
+        Todos
+      </v-btn>
+      <v-btn small class="red darken-1">
+        <v-icon> mdi-cancel </v-icon>
+        Cancelados
+      </v-btn>
+      <v-btn small class="green darken-1">
+        <v-icon> mdi-check-bold </v-icon>
+        Entregados
+      </v-btn>
+      <v-btn small class="blue accent-1">
+        <v-icon> mdi-truck-fast </v-icon>
+        Regresados
+      </v-btn>
+      <v-btn small class="purple darken-1">
+        <v-icon> mdi-lock-clock </v-icon>
+        Reservados
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "HistoryController",
+};
+</script>
+
+<style lang="css" scoped></style>
--- src/components/history/HistoryLayout.vue
+++ src/components/history/HistoryLayout.vue
@@ -1,36 +1,7 @@
 <template>
   <v-row dense>
     <v-col cols="12">
-      <v-card class="mx-auto" color="main" dark>
-        <v-card-title class="text-h5">
-          Historial de compras de {{ this.$store.state.user }}
-        </v-card-title>
-        <v-card-text>
-          Aquí puedes consultas tu hisorial de compras
-        </v-card-text>
-        <v-card-actions>
-          <v-btn small class="yellow darken-3">
-            <v-icon> mdi-script-text </v-icon>
-            Todos
-          </v-btn>
-          <v-btn small class="red darken-1">
-            <v-icon> mdi-cancel </v-icon>
-            Cancelados
-          </v-btn>
-          <v-btn small class="green darken-1">
-            <v-icon> mdi-check-bold </v-icon>
-            Entregados
-          </v-btn>
-          <v-btn small class="blue accent-1">
-            <v-icon> mdi-truck-fast </v-icon>
-            Regresados
-          </v-btn>
-          <v-btn small class="purple darken-1">
-            <v-icon> mdi-lock-clock </v-icon>
-            Reservados
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <history-controller />
     </v-col>
     <v-col class="mx-auto">
       <div class="font-weight-bold ml-8 mb-2">Historico</div>
@@ -40,9 +11,10 @@
 </template>
 
 <script>
+import HistoryController from "./HistoryController.vue";
 import HistoryLine from "./HistoryLine.vue";
 export default {
-  components: { HistoryLine },
+  components: { HistoryLine, HistoryController },
   name: "HistoryLayout",
 };
 </script>

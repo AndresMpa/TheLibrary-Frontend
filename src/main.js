a47556975f2ad61460c7985a02d6bc0c374a5dbb
Andres M. Prieto <andresprieto2703@gmail.com>
2022-05-04 14:18:44 -0500

    SweetAlert
    Adding SA config

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import vuetify from './plugins/vuetify'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import VueSweetalert2 from "vue-sweetalert2";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  VueSweetalert2,
+  render: (h) => h(App),
+}).$mount("#app");

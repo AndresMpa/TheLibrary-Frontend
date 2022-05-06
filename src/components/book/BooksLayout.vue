5b4dfb390564235d3f845e389842935e12f0e04c
Andres M. Prieto <andresprieto2703@gmail.com>
2022-05-06 14:59:11 -0500

    Footer
    Adding a footer, also adding it to Books layout

--- src/components/book/BooksLayout.vue
+++ src/components/book/BooksLayout.vue
@@ -4,17 +4,19 @@
     <LastBook />
     <MostSold />
     <Shelf />
+    <Footer />
   </article>
 </template>
 
 <script>
-import ShoppingBagLayout from '../util/ShoppingBagLayout.vue';
+import ShoppingBagLayout from "../util/ShoppingBagLayout.vue";
+import Footer from "../util/Footer.vue";
 import LastBook from "./LastBook.vue";
 import MostSold from "./MostSold.vue";
 import Shelf from "./Shelf.vue";
 export default {
   name: "BooksLayout",
-  components: { Shelf, LastBook, MostSold, ShoppingBagLayout },
+  components: { Shelf, LastBook, MostSold, ShoppingBagLayout, Footer },
 };
 </script>
 
---
+++ src/components/util/Footer.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-footer dark padless>
+    <v-card flat tile class="alter lighten-1 white--text text-center">
+      <v-card-text>
+        <v-btn
+          v-for="ref in references"
+          class="mx-4 white--text"
+          :key="ref"
+          icon
+        >
+          <a :href="ref.link">
+            <v-icon size="24px">
+              {{ ref.icon }}
+            </v-icon>
+          </a>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        Todos los derechos del material contenido es propiedad de los autores,
+        la Universidad Tecnologica de Pereira no tiene relación con este
+        proyecto más allá de ser la entiedad que ha impartido la materia a la
+        cual ha sido presentada este software, toda relación ha sido tratada de
+        manera discreta por los estudiantes y el docente que ha solicitado esta
+        u otras entregas
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} —
+        <strong>
+          <a href="https://github.com/Dreamland-Code"> Dreamland Code </a>
+        </strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data: () => ({
+    references: [
+      {
+        icon: "mdi-instagram",
+        link: "https://www.instagram.com/utpereira/",
+      },
+      {
+        icon: "mdi-facebook",
+        link: "https://www.facebook.com/utpereira",
+      },
+
+      {
+        icon: "mdi-web-plus",
+        link: "https://www.utp.edu.co/",
+      },
+    ],
+  }),
+};
+</script>

a61d190b122c33c36a75e0144ffb0d0487fb989b
Andres M. Prieto <andresprieto2703@gmail.com>
2022-05-16 00:38:31 -0500

    Remove
    Books can be removed

--- src/components/manager/RemoveBookForm.vue
+++ src/components/manager/RemoveBookForm.vue
@@ -4,7 +4,9 @@
       <span class="text-h5">Formulario de eliminación de ejemplares</span>
     </v-card-title>
     <v-card-text>
-      ¿Estas seguro de que deseas eliminar el ejemplar {{ book }}?
+      ¿Estas seguro de que deseas eliminar el ejemplar {{ book.title }}({{
+        book.issn
+      }})?
     </v-card-text>
 
     <v-card-actions>
@@ -18,6 +20,8 @@
 </template>
 
 <script>
+import axios from "axios";
+import Swal from "sweetalert2";
 export default {
   name: "RemoveBookForm",
   props: {
@@ -25,7 +29,28 @@ export default {
   },
   methods: {
     deleteItem(item) {
-      console.log(item);
+      axios
+        .delete("book/remove", {
+          issn: item.issn,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            Swal.fire({
+              title: "Eliminación exitosa",
+              text: `Se eliminó el ejemplar ${item.issn}`,
+              icon: "success",
+              confirmButtonText: "Confirmar",
+            });
+          } else {
+            Swal.fire({
+              title: "Error",
+              text: `El ejemplar ${item} no ha podido ser eliminado`,
+              icon: "error",
+              confirmButtonText: "Confirmar",
+            });
+          }
+        });
+      this.closeDeleteForm();
     },
     closeDeleteForm() {
       this.$store.dispatch("handleCrudForms", 1);

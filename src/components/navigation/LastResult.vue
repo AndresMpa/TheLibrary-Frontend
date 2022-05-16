69f012f6b3846e304e4b2b9d334ffe34418515bd
Andres M. Prieto <andresprieto2703@gmail.com>
2022-05-16 02:13:01 -0500

    Search
    Handling search for users

--- src/components/navigation/LastResult.vue
+++ src/components/navigation/LastResult.vue
@@ -1,33 +1,43 @@
 <template>
-  <v-container fluid>
+  <v-container v-if="this.empty">
+    <images :book="this.noData"></images>
+  </v-container>
+  <v-container fluid v-else>
     <h1 class="headline h1">Resultador para: {{ this.$store.state.search }}</h1>
     <Shelf :stock="result" />
   </v-container>
 </template>
 
 <script>
+import Images from "../book/Images.vue";
 import Shelf from "../book/Shelf.vue";
+import axios from "axios";
 export default {
   name: "LastResult",
-  components: { Shelf },
+  components: { Shelf, Images },
   data: () => ({
+    noData: "https://picsum.photos/id/11/500/300",
+    //"https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3D%2522404%2Berror%2522&psig=AOvVaw27JMzd_EllfBwj5RC49QIU&ust=1652770623313000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNjs68K44_cCFQAAAAAdAAAAABAg",
+    empty: true,
     result: [],
   }),
   methods: {
     searchQuery() {
-      this.result = [
-        [
-          {
-            rating: 4.5,
-            votes: "450",
-            title: "El príncipe",
-            author: "Nicolás Maquiavelo",
-            asset: "https://picsum.photos/id/11/500/300",
-            summary:
-              "El príncipe es un tratado político del siglo XVI del diplomático y teórico político italiano Nicolás Maquiavelo. Según la correspondencia de Maquiavelo, una versión parece haber sido distribuida en 1513, usando el título en latín De Principatibus",
-          },
-        ],
-      ];
+      axios
+        .post("book/search", {
+          query: this.$store.state.search,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            this.empty = false;
+            return res.data;
+          } else {
+            this.empty = true;
+          }
+        })
+        .then((data) => {
+          this.result = data;
+        });
     },
   },
   created() {

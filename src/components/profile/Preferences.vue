b98874d566874cf6b57e2611256dc6c74424d3fa
Andres M. Prieto <andresprieto2703@gmail.com>
2022-05-26 00:51:37 -0500

    Update info
    Usuers can edit their information on the front client

--- src/components/profile/Preferences.vue
+++ src/components/profile/Preferences.vue
@@ -30,16 +30,9 @@
     </v-row>
     <v-row>
       <v-col>
-        <v-switch color="primary" v-model="news">
-          <template v-slot:label> Suscribirse al sistema de noticias </template>
-        </v-switch>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
         <v-text-field
           label="Nombre de usuario"
-          v-model="username"
+          v-model="userName"
           required
         ></v-text-field>
       </v-col>
@@ -60,24 +53,51 @@
       <v-col>
         <v-text-field
           label="Correo electronico"
-          v-model="email"
+          v-model="mail"
           required
         ></v-text-field>
       </v-col>
     </v-row>
+
+    <v-row>
+      <v-col>
+        <v-text-field
+          label="Foto de perfil"
+          v-model="profile"
+          required
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-switch color="primary" v-model="news">
+          <template v-slot:label> Suscribirse al sistema de noticias </template>
+        </v-switch>
+      </v-col>
+    </v-row>
+    <div class="d-block mx-auto">
+      <v-btn color="main" @click="updateUserInformation()" dark>
+        Actualizar
+      </v-btn>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import Swal from "sweetalert2";
 export default {
   name: "Preferences",
   data() {
     return {
-      news: false,
-      username: "",
-      email: "",
+      userName: "",
       address: "",
       phone: "",
+      mail: "",
+      news: false,
+      genders: "",
+      profile: "",
       genders: [],
       preferences: [
         "Programación",
@@ -97,6 +117,56 @@ export default {
         ? "Generos favoritos"
         : "Escoga algunos generos";
     },
+  },
+  methods: {
+    updateUserInformation() {
+      axios
+        .post("/user/update", {
+          name: this.name,
+          lastName: this.lastName,
+          userName: this.userName,
+          password: this.password,
+          address: this.address,
+          phone: this.phone,
+          mail: this.mail,
+          news: this.news,
+          preferences: this.genders,
+          profile: this.profile,
+        })
+        .then((response) => response.data)
+        .then((data) => {
+          if (data.message === "La cuenta ha sido actualizada") {
+            Swal.fire({
+              title: "Cuenta actualizada",
+              text: "Se ha actualizado la información de la tu cuenta",
+              icon: "success",
+              confirmButtonText: "Entendido",
+            });
+          }
+        });
+    },
+    getUserInformation() {
+      axios
+        .post("/user/user-info", {
+          username: this.$store.state.user.information.username,
+        })
+        .then((responde) => responde.data)
+        .then((data) => {
+          this.name = data.name;
+          this.lastName = data.lastName;
+          this.userName = data.userName;
+          this.password = data.password;
+          this.address = data.address;
+          this.phone = data.phone;
+          this.mail = data.mail;
+          this.news = data.news;
+          this.genders = data.preferences;
+          this.profile = data.profile;
+        });
+    },
+  },
+  created() {
+    this.getUserInformation();
   },
 };
 </script>
